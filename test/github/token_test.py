53a45bf15a9b37d512d6315d848b56f4a97c6be1
Gyojun Youn <youngyojun@gmail.com>
2023-08-13 01:55:32 +0900

    test: Add github Token unit tests

---
+++ test/github/token_test.py
@@ -0,0 +1,31 @@
+import pytest
+
+from polytope.github import Token
+
+
+def test_empty_token():
+    with pytest.raises(AssertionError):
+        Token(token='')
+
+
+def test_int_token():
+    with pytest.raises(TypeError):
+        Token(token=3)
+
+
+def test_length_one_token():
+    token = Token('A')
+    assert 'A' == token.token
+
+
+def test_valid_token():
+    key = 'eyJpc3MiOiJ2ZWxvcGVydC5jb20iLCJleHAiOiIxNDg1MjcwMDA'
+    token = Token(token=key)
+    assert key == token.token
+
+
+def test_token_readonly():
+    token = Token('exampleTokenValue')
+
+    with pytest.raises(AttributeError):
+        token.token = 'anotherTokenValue'
