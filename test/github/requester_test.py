7c3a45debd53cc98d28dca0098473e52d6b9c74e
Gyojun Youn <youngyojun@gmail.com>
2023-08-15 23:40:04 +0900

    test: Use MockRequester instead of injection

--- test/github/requester_test.py
+++ test/github/requester_test.py
@@ -1,14 +1,14 @@
 import pytest
-import requests
 
-from polytope.github import Token, RequestMethod
-from polytope.github.Requester import Requester, RequesterDebugger
+from polytope.github import Token, RequestMethod, Requester
+from polytope.github.Requester import MockSession
 
 
 def test_auth_injection():
     token = Token('token-key-value')
-    requester = RequesterDebugger(token, 'https://api.github.com')
-    assert requester.session.headers['Authorization'] == token.token
+    requester = Requester(token, 'https://api.github.com', MockSession)
+    session: MockSession = requester.session
+    assert session.headers['Authorization'] == token.token
 
 
 def test_invalid_api_url():
@@ -21,21 +21,13 @@ def test_invalid_api_url():
 
 def test_get_user_method():
     token = Token('token-key-value')
-
-    def inject_method(
-        method: RequestMethod,
-        url: str,
-        **kwargs,
-    ) -> requests.Response:
-        return requests.Response()
-
-    requester = RequesterDebugger(token, 'https://api.github.com', inject_method)
-
+    requester = Requester(token, 'https://api.github.com', MockSession)
     requester.request(RequestMethod.GET, '/user')
 
-    assert 1 == len(requester.logs)
+    session: MockSession = requester.session
 
-    log_entry = requester.logs[0]
+    assert 1 == len(session.logs)
 
+    log_entry = session.logs[0]
     assert 'https://api.github.com/user' == log_entry.url
     assert RequestMethod.GET == log_entry.method
