1f51af7b79197f5674466387976103dbbd401182
changki <changki@moloco.com>
2023-08-20 04:45:32 +0900

    remove local test from main.py

--- src/polytope/main.py
+++ src/polytope/main.py
@@ -15,32 +15,3 @@ def load_environment():
 
 if __name__ == "__main__":
     load_environment()
-
-    from polytope.github.repository import GithubRepository
-    from polytope.github.Token import Token
-
-    ghr = GithubRepository(
-        "TAMREF",
-        "test_polytope",
-        Token(GITHUB_TOKEN)
-    )
-
-    print("================")
-    res = ghr.create()
-    print(res.status_code)
-    print(res.content)
-
-    print("================")
-    res2 = ghr.get()
-    print(res2.status_code)
-    print(res2.content)
-
-    print("================")
-    res3 = ghr.update(GithubRepositoryConfig("test_polytope_name_changed"))
-    print(res3.status_code)
-    print(res3.content)
-
-    print("================")
-    res4 = ghr.delete()
-    print(res4.status_code)
-    print(res4.content)
