3bc38b99025062ac0591e993cae27ae6199fb62e
justiceHui <justiceHui@naver.com>
2023-08-22 04:25:37 +0900

    test: Add uuidgen unit test

---
+++ test/core/uuidgen_test.py
@@ -0,0 +1,21 @@
+import pytest
+
+import polytope.core.uuidgen as uuidgen
+
+
+def test_uuid():
+    assert len(uuidgen.uuid('abcde', 3)) == 3
+
+    gen = uuidgen.PolytopeUUID(alphabet='abcde', length=15)
+    assert gen.alphabet == 'abcde'
+    assert gen.length == 15
+
+    gen.length = 5
+    uuid = gen.uuid()
+    assert len(uuid) == 5
+    for c in uuid:
+        assert c in gen.alphabet
+
+    gen = uuidgen.PolytopeUUID()
+    assert len(gen.alphabet) == 32
+    assert gen.length == 8
