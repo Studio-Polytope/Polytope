a486a950c33d84ced3902dcb2de13fffa4ad4c67
Gyojun Youn <youngyojun@gmail.com>
2023-08-28 16:46:11 +0900

    style: Auto-style by 'black'

--- test/github/repository/response_test.py
+++ test/github/repository/response_test.py
@@ -7,22 +7,22 @@ from polytope.github.repository.Repository import fetch_message_and_errors
 def test_fetch_message_and_errors_empty():
     resp = requests.Response()
     resp._content = ''
-    
+
     error_msg, errors_detail = fetch_message_and_errors(resp)
-    
+
     assert error_msg is None
     assert errors_detail is None
 
     resp._content = '{}'
 
     error_msg, errors_detail = fetch_message_and_errors(resp)
-    
+
     assert error_msg is None
     assert errors_detail is None
 
 def test_fetch_message_and_errors_normal():
     resp = requests.Response()
-    
+
     msg = 'test error msg'
     errors = [{'error_1' : 'error_desc_1', 'error_2' : 'error_desc_2'}]
     resp._content = json.dumps({
