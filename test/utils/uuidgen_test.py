e744c407281df2ded6afffc0d69b7da20cac3cde
Gyojun Youn <youngyojun@gmail.com>
2023-08-30 18:17:08 +0900

    test: Add more various tests

--- test/utils/uuidgen_test.py
+++ test/utils/uuidgen_test.py
@@ -8,24 +8,22 @@ def test_uuid_collision():
     uuids = [uuidgen.uuid() for _ in range(10000)]
     assert len(uuids) == len(set(uuids))
 
-def test_uuid():
-    assert len(uuidgen.uuid('abcde', 3)) == 3
-
-    gen = uuidgen.PolytopeUUID(alphabet='abcde', length=15)
-    assert gen.alphabet == 'abcde'
-    assert gen.length == 15
-
-    gen.length = 5
-    uuid = gen.uuid()
-    assert len(uuid) == 5
-    for c in uuid:
-        assert c in gen.alphabet
-
+def test_default():
     gen = uuidgen.PolytopeUUID()
     assert len(gen.alphabet) == 32
     assert gen.length == 8
 
-def test_large_uuid():
+def test_gen_uuid():
+    gen = uuidgen.PolytopeUUID(alphabet='0123456789', length=15)
+    assert gen.alphabet == '0123456789'
+    assert gen.length == 15
+
+    gen.length = 15
+    uuid = gen.uuid()
+    assert len(uuid) == 15
+    for c in uuid:
+        assert c in gen.alphabet
+
     gen = uuidgen.PolytopeUUID(length=300)
     assert len(gen.alphabet) == 32
     assert gen.length == 300
@@ -36,23 +34,7 @@ def test_large_uuid():
         for c in uuid:
             assert c in gen.alphabet
 
-def test_bulk():
-    uuids = uuidgen.uuid_bulk(9, 'abc', 2)
-    for uuid in uuids:
-        assert len(uuid) == 2
-        for c in uuid:
-            assert c in 'abc'
-    assert(len(uuids) == len(set(uuids)))
-
-    gen = uuidgen.PolytopeUUID('abc', 2)
-    uuids = gen._PolytopeUUID__uuid_bulk_large(4)
-    for uuid in uuids:
-        assert len(uuid) == 2
-        for c in uuid:
-            assert c in 'abc'
-    assert(len(uuids) == len(set(uuids)))
-
-def test_large_bulk():
+def test_gen_bulk():
     gen = uuidgen.PolytopeUUID(length=300)
 
     uuids = gen.uuid_bulk(3000)
@@ -64,16 +46,54 @@ def test_large_bulk():
         for c in uuid:
             assert c in gen.alphabet
 
+def test_uuid():
+    alphabet = '0123456789'
+    uuid = uuidgen.uuid(alphabet, 12)
+    assert len(uuid) == 12
+    for c in uuid:
+        assert c in alphabet
+
+    for _ in range(300):
+        uuid = uuidgen.uuid(alphabet, 300)
+        assert len(uuid) == 300
+        for c in uuid:
+            assert c in alphabet
+
+def test_bulk():
+    alphabet = '0123456789'
+
+    uuids = uuidgen.uuid_bulk(count=3000, alphabet=alphabet, length=300)
+    assert len(uuids) == 3000
+    assert len(uuids) == len(set(uuids))
+
+    for uuid in uuids:
+        assert len(uuid) == 300
+        for c in uuid:
+            assert c in alphabet
+
 def test_value_error():
+    # Too few alphabets
     with pytest.raises(ValueError):
-        gen = uuidgen.PolytopeUUID(alphabet='aa')
+        gen = uuidgen.PolytopeUUID(alphabet="")
+    with pytest.raises(ValueError):
+        gen = uuidgen.PolytopeUUID(alphabet="abc")
+    with pytest.raises(ValueError):
+        gen = uuidgen.PolytopeUUID(alphabet="012345678")
 
+    # Duplicated alphabets
     with pytest.raises(ValueError):
-        uuid = uuidgen.uuid('abcd', 0)
+        gen = uuidgen.PolytopeUUID(alphabet='aaabbbcccdddeee')
+    with pytest.raises(ValueError):
+        gen = uuidgen.PolytopeUUID(alphabet='bcdAefghijklmAno')
 
+    # Too small length
     with pytest.raises(ValueError):
-        uuids = uuidgen.uuid_bulk(10, 'abc', 2)
+        uuid = uuidgen.uuid('0123456789', -1)
+    with pytest.raises(ValueError):
+        uuid = uuidgen.uuid('0123456789', 0)
+    with pytest.raises(ValueError):
+        uuid = uuidgen.uuid('0123456789', 4)
 
+    # Too large count
     with pytest.raises(ValueError):
-        gen = uuidgen.PolytopeUUID(alphabet='abc', length=2)
-        uuids = gen._PolytopeUUID__uuid_bulk_large(5)
+        uuids = uuidgen.uuid_bulk(1100, '0123456789', 5)
